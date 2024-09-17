0e348300252fbec0f8b0987f16856596148da564
Szaniszló Árpád (MArpadszF) <developedbybarley@gmail.com>
2024-09-17 11:52:55 +0200

    Heade phone layout done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { IoMdStar } from 'react-icons/io'
+import profile from '../assets/images/profile.jpg'
 
 export const Header = () => {
-  return (
-    <header className='md:grid grid-cols-2 mt-5'>
-        <div className='bg-red-400'>First</div>
-        <div className='bg-orange-50'>Second</div>
-        
-    </header>
-  )
+    return (
+        <header className='bg-mainDark'>
+            {/* Left section with profile and stars */}
+            <div className='bg-yellow-400 p-5'>
+                <div>
+                    <img src={profile} alt="Profile" className='rounded-full' />
+                    <div className='flex items-center justify-center my-5'>
+                        <IoMdStar size={50} />
+                        <IoMdStar size={50} />
+                        <IoMdStar size={50} />
+                        <IoMdStar size={50} />
+                        <IoMdStar size={50} />
+                    </div>
+                </div>
+            </div>
+
+            {/* Right section with name and profession */}
+            <div className='bg-red-500'>
+                <div className='text-center space-y-2 py-10'>
+                    <h1 className='font-pricedown text-7xl'>SZANISZLÓ ÁRPÁD</h1>
+                    <h2 className='font-pricedown text-3xl'>FULL STACK WEB DEVELOPER</h2>
+                </div>
+            </div>
+        </header>
+    )
 }
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,5 +1,12 @@
+import { Header } from "../components/Header";
+
 export default function Main() {
     return (
-        <h1>Main</h1>
+        <>
+            <Header />
+            <div className="bg-mainLightDark py-10">
+                Alert
+            </div>
+        </>
     )
 }
